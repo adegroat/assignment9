2dde38637bf44aefa4ab2ba1a2dfa29a9e3d6b08
AlexLinux <adegroat917@gmail.com>
2014-04-24 01:44:14 -0600

    Created test setup

--- AddressBook.c
+++ AddressBook.c
@@ -6,5 +6,26 @@ int main()
 	int numAddresses = 0;
 	int userInput = 0;
 
+	do
+	{
+		userInput = 0;
+		printf("1. Enter Address\n2. List Addresses(%d total)\n3. Quit\n", numAddresses);
+		scanf("%1d", &userInput);
+
+		fflush(stdin);
+
+		if(userInput == 1)
+		{
+			printf("Address: ");
+			fgets(addresses[numAddresses], 29, stdin);
+		}
+
+		if(userInput == 2)
+		{
+
+		}
+
+	} while(numAddresses < 10 && userInput != 3);
+
 	return 0;
 }
